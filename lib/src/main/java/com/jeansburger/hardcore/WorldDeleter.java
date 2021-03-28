365c006e79a08f5563a93c1fc732dece76b9fa86
Doni Crosby <doni.crosby1995@gmail.com>
2021-03-28 14:33:35 -0400

    Added Recreate World Runnable, need to find a better way to handle new world creation

--- lib/src/main/java/com/jeansburger/hardcore/WorldDeleter.java
+++ lib/src/main/java/com/jeansburger/hardcore/WorldDeleter.java
@@ -1,27 +1,22 @@
 package com.jeansburger.hardcore;
 import com.jeansburger.hardcore.MultiPlayerHardcore;
 import com.jeansburger.hardcore.interfaces.observers.*;
+import com.jeansburger.hardcore.interfaces.runnable.RecreateWorldRunnable;
 
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import java.util.logging.Logger;
 import java.util.*;
 import java.io.*;
 
-public class WorldDeleter extends BukkitRunnable
-                          implements PlayerDeathObserver,
-                          NewWorldGeneratedObserver {
+public class WorldDeleter implements
+  PlayerDeathObserver, NewWorldGeneratedObserver
+{
   private MultiPlayerHardcore parent;
   private Logger logger;
   private BukkitTask deleteWorldTask;
+  private RecreateWorldRunnable deleteWorldRunnable;
 
-  private List<World> worlds = new ArrayList<World>();
-  private List<WorldCreator> newWorlds = new ArrayList<WorldCreator>();
-  private long newSeed;
   private boolean deleteWorld = false;
 
   public WorldDeleter(MultiPlayerHardcore parent) {
@@ -32,46 +27,22 @@ public class WorldDeleter extends BukkitRunnable
   @Override
   public void onPlayerDeath(PlayerDeathEvent e){
     if (!deleteWorld){
-      if (this.deleteWorldTask != null) {
-        this.deleteWorldTask.cancel();
-      }
-      this.deleteWorldTask = this.runTask(this.parent);
+      this.deleteWorldRunnable = new RecreateWorldRunnable(this, this.getWorldName());
+      this.deleteWorldTask = this.deleteWorldRunnable.runTask(this.parent);
     }
     this.deleteWorld = true;
   }
 
   @Override
   public void onNewWorldCreate(){
+    if (this.deleteWorldTask != null) {
+      this.deleteWorldTask.cancel();
+    }
     this.deleteWorld = false;
   }
 
-  @Override
-  public void run() {
-    String worldName = this.getWorldName();
-    updateWorldsList(worldName);
-    for (World oldWorld: this.worlds){
-      WorldCreator wc = new WorldCreator(oldWorld.getName());
-      wc.copy(oldWorld);
-      this.newWorlds.add(wc);
-      unloadWorld(oldWorld);
-      deleteWorld(oldWorld.getWorldFolder());
-    }
-    this.newSeed = new Random().nextLong();
-    for (WorldCreator wc: this.newWorlds){
-      wc.seed(this.newSeed);
-      wc.createWorld();
-    }
-    Bukkit.reloadData();
-    this.parent.notifyNewWorldGenerated();
-    this.cancel();
-
-  }
-
-  private void updateWorldsList(String worldName) {
-    this.logger.info("Updateing world list for world: " + worldName);
-    this.worlds.add(Bukkit.getWorld(worldName));
-    this.worlds.add(Bukkit.getWorld(worldName + "_nether"));
-    this.worlds.add(Bukkit.getWorld(worldName + "_the_end"));
+  public Logger getLogger() {
+    return parent.getLogger();
   }
 
   private String getWorldFromServerProps(){
@@ -96,25 +67,8 @@ public class WorldDeleter extends BukkitRunnable
     return this.getWorldFromServerProps();
   }
 
-  private void unloadWorld(World world) {
-    if(!world.equals(null)) {
-        Bukkit.getServer().unloadWorld(world, true);
-    } else {
-      this.logger.warning("World " + world + " could not be unloaded!");
-    }
+  public void notifyNewWorldGenerated(){
+    this.parent.notifyNewWorldGenerated();
   }
 
-  private boolean deleteWorld(File path) {
-    if(path.exists()) {
-      File files[] = path.listFiles();
-      for(int i=0; i<files.length; i++) {
-        if(files[i].isDirectory()) {
-          deleteWorld(files[i]);
-        } else {
-          files[i].delete();
-        }
-      }
-    }
-    return(path.delete());
- }
 }
